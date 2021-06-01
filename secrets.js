0c6decf8233e9da8f07c968680c6712ab986eff7
KellyEgesa <kelly.egesa@gmail.com>
2021-06-01 19:49:55 +0300

    Finished

---
+++ secrets.js
@@ -0,0 +1,8 @@
+//should be hidden but exposed for others to consume
+
+module.exports.jwtPrivateKey = "backend-to-do@17380";
+module.exports.Client_ID =
+  "401039850648-fq1luej8lk7r0dbfm8bho5lpa90ai17n.apps.googleusercontent.com";
+module.exports.Client_Secret = "Fs4jPkWEWu5kRr7Oy4e0Q-0y";
+module.exports.Refresh_Token =
+  "1//040Fu5Rd93NEFCgYIARAAGAQSNwF-L9Ir-f7dPOIRj16wHgnIe6mA79LkP8Q-UDhj3M0Fuady3zSmW9Z5mEx3kUwz2Kc6DJ_uUMA";
